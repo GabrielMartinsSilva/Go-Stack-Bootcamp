09b29578e214cc99a63e69b364f344b38cfdcc7f
Gabriel <gamartins77@gmail.com>
2019-09-10 22:42:28 -0300

    tratamento de exceções ok

--- Modulo03/src/config/mail.js
+++ Modulo03/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'ad6d41ffe88f27',
-    pass: 'd879d47d1f6695',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com ',
